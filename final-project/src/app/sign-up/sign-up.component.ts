a2ead2f888e0c8651518c2206882999e423f8dd4
Nazar Ali <nazaralibaloch3@gmail.com>
2024-12-01 11:32:46 +0500

    fixing some routing issue

--- final-project/src/app/sign-up/sign-up.component.ts
+++ final-project/src/app/sign-up/sign-up.component.ts
@@ -15,5 +15,64 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  name:string='';
+  email:string='';
+  password:string='';
+  conf_password:string='';
+  nameMsg='';
+  emailMsg='';
+  passwordMsg='';
+  conf_passwordMsg='';
+  formMsg='';
 
+  validator() {
+    if(this.nameMsg == '  ' && this.emailMsg == '  ' && this.passwordMsg == '  ' && this.conf_passwordMsg == '  '){
+      this.formMsg=''
+    } else {
+      this.formMsg='Some Credentials are Wrong';
+    }
+  }
+
+  checkName() {
+    if(this.name==''){
+      this.nameMsg = 'Name is required.';
+    }
+    else if(this.name.length<4){
+      this.nameMsg = 'Maximum 4 characters long.';
+    }
+    else {
+      this.nameMsg = '  ';
+    }
+  }
+  checkEmail(){
+    const pattern="^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+    if(this.email==''){
+      this.emailMsg = 'Email is required.';
+    }
+    else if(!this.email.match(pattern)){
+      this.emailMsg='Invalid email address.';
+    }
+    else{
+      this.emailMsg = '  ';
+    }
+  }
+  checkPassword(){
+    if(this.password=='') {
+      this.passwordMsg = 'Password is required.'
+    }
+    else if(this.password.length<8){
+      this.passwordMsg = 'Maximum 8 characters long.';
+    }
+    else{
+      this.passwordMsg = '  ';
+    }
+  }
+  checkPassword_match() {
+   if(this.password != this.conf_password){
+     this.conf_passwordMsg='Password does not match.';
+   }
+   else{
+     this.conf_passwordMsg = '  ';
+   }
+  }
 }
