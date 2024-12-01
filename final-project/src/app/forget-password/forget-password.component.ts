75ba6bce777556528073c0f527fa39bd79b3944d
Nazar Ali <nazaralibaloch3@gmail.com>
2024-12-01 10:27:02 +0500

    fix routing issue

--- final-project/src/app/forget-password/forget-password.component.ts
+++ final-project/src/app/forget-password/forget-password.component.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-forget-password',
   standalone:true,
-  imports:[ReactiveFormsModule,FormsModule],
+  imports:[ReactiveFormsModule,
+    FormsModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './forget-password.component.html',
   styleUrls: ['./forget-password.component.css']
 })
 export class ForgetPasswordComponent {
-  email: string = ''; 
+  email: string = '';
+  email_msg='';
 
   constructor(private router: Router) {}
 
   onSubmit() {
-    console.log('Email:', this.email);
+    const patternEmail="^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+    if(this.email==''){
+      this.email_msg = 'Email is required.';
+    }
+    else if(!this.email.match(patternEmail)){
+      this.email_msg='Invalid email address.';
+    }
+    else {
+      this.email_msg = '';
 
-    this.router.navigate(['/verify-token']);
+      console.log('Email:', this.email);
+      this.router.navigate(['/verify-token']);
+    }
   }
 }
